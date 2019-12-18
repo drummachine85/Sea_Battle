9a0c80fdf775e42670e28e7363b023e90b24b616
drummachine85 <asmevs@gmail.com>
2019-12-18 13:55:59 +0200

    Update

--- Sea_Battle.cpp
+++ Sea_Battle.cpp
@@ -101,6 +101,7 @@ void user(char user_name[80])
 void insert(int arry[12][12],int choise)   //Insert ship coordinates
 {
 	char a;
+	char b=(-3);
 	int row;
 	int colmn;
 	bool y_n=true;
@@ -109,9 +110,24 @@ void insert(int arry[12][12],int choise)   //Insert ship coordinates
 	do
 	{
 		cout << "Vvedite coordinati" << endl;
-
-		cout << "Po vertikali ot 1 do 10: ";
-		cin >> colmn;
+		
+		for(int i = 0;i < 1;i++) 
+		{	cout << "Po vertikali ot 1 do 10: ";
+				cin >> colmn;
+				if(cin.fail())
+				{
+					
+					cout << "Warning" << endl;
+					cout << "You entered unknown symbol !!! " << endl;
+					cin.clear();
+					cin.ignore(32767, '\n');
+					i--;
+                }
+				
+				if (colmn < 1) { cout << "Please enter number >0 <11" << endl; };
+				if (colmn >= 11) { cout << "Please enter number >0  <11" << endl; };
+				
+		}
 
 		cout << "Po gorizontali ot A do J: ";
 		cin >> a;
@@ -232,13 +248,3 @@ int charTostring(char cordinates)
 }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
